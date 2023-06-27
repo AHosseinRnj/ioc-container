36a65c73b65fc822cf6b03884014a9bfb5e968af
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-06-27 20:01:37 +0330

    Implemented IIocContainer interface in IocContainer class

---
+++ ioc-container/IocContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ioc_container
+{
+    public class IocContainer : IIocContainer
+    {
+        public void Register<TInterface, TImplementation>() where TInterface : class
+                                                            where TImplementation : class, TInterface
+        {
+            throw new NotImplementedException();
+        }
+        public TInterface Resolve<TInterface>() where TInterface : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
