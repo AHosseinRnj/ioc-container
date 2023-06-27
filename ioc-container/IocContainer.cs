7ba39ea9e074f2d656e758152ac7482d15e86d9f
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-06-27 20:16:01 +0330

    Implemented Register method

--- ioc-container/IocContainer.cs
+++ ioc-container/IocContainer.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ioc_container
 {
     public class IocContainer : IIocContainer
     {
+        private readonly Dictionary<Type, Type> _map = new Dictionary<Type, Type>();
+
         public void Register<TInterface, TImplementation>() where TInterface : class
                                                             where TImplementation : class, TInterface
         {
-            throw new NotImplementedException();
+            if (_map.ContainsKey(typeof(TInterface)))
+                throw new InvalidOperationException($"{typeof(TInterface).FullName} is already exists.");
+
+            _map.Add(typeof(TInterface), typeof(TImplementation));
         }
+
         public TInterface Resolve<TInterface>() where TInterface : class
         {
             throw new NotImplementedException();
