4f829eb1f2da3082bc5f0e90e8bd64ccdc23cb1e
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-06-27 23:44:40 +0330

    Added dependency injection using IoC container

--- ioc-container/Program.cs
+++ ioc-container/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ioc_container
+﻿namespace ioc_container
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var iocContainer = new IocContainer();
+
+            iocContainer.Register<ILogger, Logger>();
+            iocContainer.Register<ITester, Tester>();
+
+            var tester = iocContainer.Resolve<ITester>();
+            tester.CompleteTest("Hello");          
         }
     }
 }
