37ded4010706afc1fbb78deeac52eedff2f29e7f
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-06-27 23:38:14 +0330

    Added and implemented ITester and Tester class

---
+++ ioc-container/ITester.cs
@@ -0,0 +1,7 @@
+﻿namespace ioc_container
+{
+    public interface ITester
+    {
+        void CompleteTest(string message);
+    }
+}
---
+++ ioc-container/Tester.cs
@@ -0,0 +1,16 @@
+﻿namespace ioc_container
+{
+    public class Tester : ITester
+    {
+        private ILogger _logger;
+        public Tester(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void CompleteTest(string message)
+        {
+            _logger.PrintMessage(message);
+        }
+    }
+}
