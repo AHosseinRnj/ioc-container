55fd1bf8e2114e67882033d2c583591acd16c708
Amirhossein Ranjbaran <16597276+AHosseinRnj@users.noreply.github.com>
2023-06-27 21:57:44 +0330

    Implemented Resolve method

--- ioc-container/IocContainer.cs
+++ ioc-container/IocContainer.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ioc_container
 {
     public class IocContainer : IIocContainer
     {
         private readonly Dictionary<Type, Type> _map = new Dictionary<Type, Type>();
+        private readonly Dictionary<object, object> _instanceMap = new Dictionary<object, object> { };
 
         public void Register<TInterface, TImplementation>() where TInterface : class
                                                             where TImplementation : class, TInterface
@@ -18,7 +20,38 @@ namespace ioc_container
 
         public TInterface Resolve<TInterface>() where TInterface : class
         {
-            throw new NotImplementedException();
+            return (TInterface)Resolve(typeof(TInterface));
         }
+
+        private object Resolve(Type typeToResolve)
+        {
+            Type implementation;
+            if (!_map.TryGetValue(typeToResolve, out implementation))
+                throw new InvalidOperationException($"No implementation added for {typeToResolve}");
+
+            if(_instanceMap.ContainsKey(implementation))
+                return _instanceMap[implementation];
+
+            // Get constructor
+            var constructor = implementation.GetConstructors().FirstOrDefault();
+            if (constructor == null)
+                Console.WriteLine($"No Constructor found for {implementation}");
+
+            // Get constructor parameters
+            var constructorParameters = constructor.GetParameters();
+            var parameters = new List<object>();
+            foreach (var parametersToResolve in constructorParameters)
+            {
+                // Resolve parameters recursivly
+                parameters.Add(Resolve(parametersToResolve.ParameterType));
+            }
+
+            var instance = Activator.CreateInstance(implementation, parameters.ToArray());
+            _instanceMap[implementation] = instance;
+
+            return instance;
+        }
+
+        public void Dispose() { }
     }
 }
